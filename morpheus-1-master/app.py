4aa2249113d0d098ce653b1949dd819b2ec86f5e
lky100047 <47895484+lky100047@users.noreply.github.com>
2022-04-15 15:32:38 +0800

    Update app.py

--- morpheus-1-master/app.py
+++ morpheus-1-master/app.py
@@ -2,7 +2,9 @@ import streamlit as st
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 from morpheus_hf.morpheus import MorpheusHuggingfaceNLI, MorpheusHuggingfaceQA, MorpheusHuggingfaceSummarization
 
-model_name = "deepset/roberta-base-squad2"
+model_name = st.selectbox(
+     'Select a qa model',
+     ('deepset/roberta-base-squad2', 'deepset/bert-base-uncased-squad2'))
 
 @st.cache(allow_output_mutation=True)
 def load_qa_model():
@@ -19,16 +21,15 @@ question = st.text_input("Questions from this article?")
 button = st.button("Get me Answers")
 with st.spinner("Discovering Answers.."):
     if button and sentence:
-        answers = qa(question=question, context=sentence)
-        st.write(answers['answer'])
+        result = qa(question=question, context=sentence)
+        st.write(result['answer'], result['score'], result['start'], result['end'])
         
-        test_morph_qa = MorpheusHuggingfaceQA('deepset/roberta-base-squad2')
+        test_morph_qa = MorpheusHuggingfaceQA(model_name)
         context = sentence
-        q_dict = {"question": question , "id": "56ddde6b9a695914005b9628", "answers": [{"text": answers['answer'], "answer_start": 159}], "is_impossible": False}
+        q_dict = {"question": question , "id": "56ddde6b9a695914005b9628", "answers": [{"text": result['answer'], "answer_start": 159}], "is_impossible": False}
 
         text = test_morph_qa.morph(q_dict, context)
         st.write(text[0])
         
         answersEdited = qa(question=text[0], context=sentence)
         st.write(answersEdited['answer'])
-        
